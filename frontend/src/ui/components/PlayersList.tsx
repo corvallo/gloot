458a47b93e7f8e064087dadbdc9a76b9a82df904
f.stallo <f.stallo@logispin.com>
2022-09-13 14:16:26 +0200

    chore: added loading state for players list

--- frontend/src/ui/components/PlayersList.tsx
+++ frontend/src/ui/components/PlayersList.tsx
@@ -3,10 +3,12 @@ import { FixedSizeGrid } from "react-window";
 import { IWithChildren } from "../../@typings/children";
 import usePlayersStore from "../../store/players";
 import Empty from "./Empty";
+import Loading from "./Loading";
 import PlayerCard from "./PlayerCard";
 
 const PlayersList: React.FC<IWithChildren> = () => {
   const players = usePlayersStore((state) => state.players);
+  const loading = usePlayersStore((state) => state.loading);
   const [height, setHeight] = useState<number>(0);
   const [w, setW] = useState<number>(0);
   const ref = useRef<HTMLDivElement>(null);
@@ -20,13 +22,13 @@ const PlayersList: React.FC<IWithChildren> = () => {
 
   return (
     <div className="list" ref={ref}>
-      {players && players.length ? (
+      {players && players.length && !loading && (
         <FixedSizeGrid height={height} width={w} columnCount={1} columnWidth={w - 100} rowCount={players.length} rowHeight={250}>
           {(props) => <PlayerCard player={players[props.rowIndex]} {...props} />}
         </FixedSizeGrid>
-      ) : (
-        <Empty />
       )}
+      {!loading && !players.length && <Empty />}
+      {loading && <Loading />}
     </div>
   );
 };
